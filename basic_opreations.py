03e8e18dee75f79c5cd664a0369c925a17a3e1dd
Samuel Bereket <samuelbereket123@gmail.com>
2025-09-29 11:28:34 +0300

    basic opreations

---
+++ basic_opreations.py
@@ -0,0 +1,24 @@
+#Addition 
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+Addition = number1 + number2
+print(f"Addition of {number1} and {number2} is: {Addition}")
+
+#Subtraction
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+Addition = number1 - number2
+print(f"Addition of {number1} and {number2} is: {Addition}")
+
+
+#Division
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+Addition = number1 / number2
+print(f"Addition of {number1} and {number2} is: {Addition}")
