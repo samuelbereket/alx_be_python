d9a2e11b451684df448beab1fe68b7aab39e896d
Samuel Bereket <samuelbereket123@gmail.com>
2025-09-30 02:25:23 +0300

    Fix variable names and print statements in basic operations

--- python_introduction/basic_opreations.py
+++ python_introduction/basic_opreations.py
@@ -1,18 +1,18 @@
-#Addition 
+#Addition
 
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 
-Addition = number1 + number2
-print(f"Addition of {number1} and {number2} is: {Addition}")
+addition_result = number1 + number2
+print(f"Addition of {number1} and {number2} is: {addition_result}")
 
 #Subtraction
 
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 
-Addition = number1 - number2
-print(f"Addition of {number1} and {number2} is: {Addition}")
+subtraction_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is: {subtraction_result}")
 
 
 #Division
@@ -20,5 +20,5 @@ print(f"Addition of {number1} and {number2} is: {Addition}")
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 
-Addition = number1 / number2
-print(f"Addition of {number1} and {number2} is: {Addition}")
+division_result = number1 / number2
+print(f"Division of {number1} and {number2} is: {division_result}")
