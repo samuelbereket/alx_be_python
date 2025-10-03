2ec9a9494cee63f3b17ea80a2a4911d89f539d41
Samuel Bereket <samuelbereket123@gmail.com>
2025-10-03 13:28:19 +0300

    Added a control-flow folder

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+
+
+ask = input("What's the weather like today? (sunny/rain/cold): ")
+
+if ask == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif ask == "rain":
+    print("Don't forget your umbrella and a raincoat.")
+elif ask == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
